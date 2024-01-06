e7a6ce00efde27ed54794a9e907938d50485e9b4
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2024-01-06 16:15:52 +0500

    authors section

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,7 +6,7 @@ const slider = new Swiper('.site-wrapper', {
 	loop: false,
 	direction: 'vertical',
 	// allowTouchMove: false,
-	initialSlide: 3,
+	// initialSlide: 4,
 	// hashNavigation: {
 	// 	replaceState: true,
 	// },
